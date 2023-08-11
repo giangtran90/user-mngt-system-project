2d884f087e8379a3fbc8794a834859049fbcc91f
ADMIN <giang08t3@gmail.com>
2023-08-11 22:21:16 +0700

    Backend: Controller: fetch all users => OK

--- user-mngt-system-res-api/src/main/java/com/hgcode/mngt/controller/UserController.java
+++ user-mngt-system-res-api/src/main/java/com/hgcode/mngt/controller/UserController.java
@@ -2,20 +2,27 @@ package com.hgcode.mngt.controller;
 
 import com.hgcode.mngt.model.User;
 import com.hgcode.mngt.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1")
 public class UserController {
-    @Autowired
+
     private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping("/users")
     public User saveUser(@RequestBody User user){
         return userService.saveUser(user);
+    }
+
+    @GetMapping("/users")
+    public List<User> getAllUsers(){
+        return userService.getAllUsers();
     }
 }
